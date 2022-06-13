51baf3cafe238ae29b424852040bee1e7a602a49
ryankopf <git@ryankopf.com>
2022-06-12 20:25:44 -0500

    Creating a new Rails CMS.

--- app/models/rhino/page.rb
+++ app/models/rhino/page.rb
@@ -17,10 +17,9 @@ module Rhino
       Publishedpage.create(url: self.url, content: self.render_content, page_id: self.id, title: self.get_title, seo_title: self.seo_title)
       self.reload!
     end
-    def render_content(t)
+    def render_content
       @no_edit_button = true
-      #ApplicationController.new.render_to_string(partial: t, locals: { page: @page })
-      ApplicationController.new.render_to_string(partial: 'rhino/pages/show', locals: { page: @page })
+      ApplicationController.new.render_to_string(partial: 'rhino/pages/show', locals: { page: self })
       #PagesController.show
       #self.blocks.pluck(:content).join('')
     end
