fb993d58454c4f1374672ac9911c653575f220ae
ryankopf <git@ryankopf.com>
2022-06-13 00:48:54 -0500

    Creating a new Rails CMS.

--- app/controllers/rhino/blocks_controller.rb
+++ app/controllers/rhino/blocks_controller.rb
@@ -27,6 +27,10 @@ module Rhino
       end
     end
 
+    def destroy
+      @block.destroy
+    end
+
     private
       # Use callbacks to share common setup or constraints between actions.
       def set_block
