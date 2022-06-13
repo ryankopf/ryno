c70d35c9f0b23d04722c403ca4936d53b0395718
ryankopf <git@ryankopf.com>
2022-06-12 21:46:25 -0500

    Creating a new Rails CMS.

--- app/controllers/rhino/pages_controller.rb
+++ app/controllers/rhino/pages_controller.rb
@@ -23,9 +23,6 @@ module Rhino
     def publish
       @page.render_content(request)
       @page.publish!
-      # respond_to do |format|
-      #   format.js { }
-      # end
       redirect_to @page
     end
 
