3661fed7de9c3f9c1221dd7b35cac4a32bad678a
ryankopf <git@ryankopf.com>
2022-06-12 20:33:44 -0500

    Creating a new Rails CMS.

--- app/controllers/rhino/publishedpages_controller.rb
+++ app/controllers/rhino/publishedpages_controller.rb
@@ -10,7 +10,7 @@ module Rhino
     private
       # Use callbacks to share common setup or constraints between actions.
       def set_publishedpage
-        @publishedpage = Publishedpage.find(params[:id])
+        @publishedpage = Publishedpage.find_by(url: params[:id])
       end
   end
 end
