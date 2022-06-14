6b7bb063f1e4c0651ae86c653fd287cab516b09c
ryankopf <git@ryankopf.com>
2022-06-13 20:24:02 -0500

    Creating a new Rails CMS.

--- app/controllers/rhino/blocks_controller.rb
+++ app/controllers/rhino/blocks_controller.rb
@@ -4,7 +4,6 @@ module Rhino
   class BlocksController < ApplicationController
     before_action :must_be_admin
     before_action :set_block, only: [:show,:update,:destroy]
-    before_action :set_page, only: [:new]
 
     # GET /blocks
     def index
@@ -23,12 +22,42 @@ module Rhino
     end
 
     def new
+      if params[:page_id]
+        new_page
+      elsif params[:block_id]
+        new_block
+      end
+    end
+
+    def new_page
+      @page = Page.find(params[:page_id])
       @block = @page.blocks.create
       respond_to do |format|
         format.html { redirect_to @block }
         format.turbo_stream {
           render turbo_stream: turbo_stream.append("page_#{@page.id}_blockform", partial: 'show', locals: {block: @block})
         }
+      end
+    end
+
+    def new_block
+      @parentblock = Block.find(params[:block_id])
+      @block = @parentblock.blocks.create
+      respond_to do |format|
+        format.html { redirect_to @block }
+        format.turbo_stream {
+          render turbo_stream: turbo_stream.append("block_#{@parentblock.id}_blockform", partial: 'show', locals: {block: @block})
+        }
+      end
+    end
+
+    def make_columns
+      if @block.blocks.size < 3
+        @block.update(block_type: 'columns', css_classes: 'row')
+        @block.blocks.create(css_classes: 'col-3')
+        @block.blocks.create(css_classes: 'col-9')
+      else
+
       end
     end
 
@@ -55,9 +84,6 @@ module Rhino
       # Use callbacks to share common setup or constraints between actions.
       def set_block
         @block = Block.find(params[:id])
-      end
-      def set_page
-        @page = Page.find(params[:page_id])
       end
 
       # Only allow a list of trusted parameters through.
