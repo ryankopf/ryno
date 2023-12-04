897a4961107770ce8e73fd18b20e9aaad713f0bb
ryankopf <git@ryankopf.com>
2023-12-03 21:15:11 -0600

    Creating new Rails CMS.

--- lib/ryno/page_constraint.rb
+++ lib/ryno/page_constraint.rb
@@ -2,6 +2,7 @@ module Ryno
   class PageConstraint
     def initialize
       @ryno_pages = []
+      # This is required because you can not run migrations if this throws an error
       if ActiveRecord::Base.connection.data_source_exists?('ryno_publishedpages')
         @ryno_pages = Publishedpage.pages
       end
